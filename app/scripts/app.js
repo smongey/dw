bd60baa4dc0120cda487b917ebf7d1ad7138e8e0
smongey <sean@aokstudio.com>
2014-12-08 21:17:22 +0000

    update image links in js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -39,7 +39,7 @@ $(document).ready(function () {
 $(window).load(function () {
 	'use strict';
 
-	var headerImages = ['/images/header1.jpg', '/images/header2.jpg', '/images/header3.jpg', '/images/header4.jpg', '/images/header5.jpg', '/images/header6.jpg', '/images/header7.jpg', '/images/header8.jpg'];
+	var headerImages = ['./images/header1.jpg', './images/header2.jpg', './images/header3.jpg', './images/header4.jpg', './images/header5.jpg', './images/header6.jpg', './images/header7.jpg', './images/header8.jpg'];
 	var randomer = Math.floor((Math.random() * headerImages.length) + 1);
 	var imageAddress = 'url(' + headerImages[randomer] + ')';
 
